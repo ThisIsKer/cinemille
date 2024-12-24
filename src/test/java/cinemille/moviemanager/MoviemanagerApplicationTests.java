46c11a2e1d3fd001e6b7dc4a1baf51fad6570a04
sebastianofilippetto <sebastiano.filippetto@targatelematics.com>
2024-12-24 11:37:45 +0100

    Initial commit

---
+++ src/test/java/cinemille/moviemanager/MoviemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cinemille.moviemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
